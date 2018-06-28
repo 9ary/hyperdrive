3db3ce990846b924203e7f9c544d1b44fbceb916
Dan Elkouby <streetwalkermc@gmail.com>
2018-06-28 16:04:12 +0300

    Make synth script more modular

--- fpga/synthesize.sh
+++ fpga/synthesize.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
@@ -15,8 +15,9 @@ basedir=$(cd "${0%/*}" && echo ${PWD})
 mkdir -p "${workdir}"
 cd "${workdir}"
 
-# XST options
-cat > "${projname}.xst" << EOF
+build() {
+    # XST options
+    cat > "${projname}.xst" << EOF
 run
 -ifn ${projname}.prj
 -ofn ${projname}
@@ -27,32 +28,34 @@ run
 -opt_level 1
 EOF
 
-# HDL file listing
-find "${basedir}/src" -iname *.vhd -fprintf "${projname}.prj" 'vhdl work "%p"\n'
+    # HDL file listing
+    find "${basedir}/src" -iname *.vhd -fprintf "${projname}.prj" 'vhdl work "%p"\n'
 
-"${XILINX_ISE}/xst" -ifn "${projname}.xst" -ofn "${projname}.syr"
+    "${XILINX_ISE}/xst" -ifn "${projname}.xst" -ofn "${projname}.syr"
 
-"${XILINX_ISE}/ngdbuild" -dd _ngo -uc "${basedir}/${ucf}" -p "${partname}-${partpackage}-${partspeed}" "${projname}.ngc" "${projname}.ngd"
+    "${XILINX_ISE}/ngdbuild" -dd _ngo -uc "${basedir}/${ucf}" -p "${partname}-${partpackage}-${partspeed}" "${projname}.ngc" "${projname}.ngd"
 
-"${XILINX_ISE}/map" -p "${partname}-${partpackage}-${partspeed}" -w -mt 2 -o "${projname}_map.ncd" "${projname}.ngd"
+    "${XILINX_ISE}/map" -p "${partname}-${partpackage}-${partspeed}" -w -mt 2 -o "${projname}_map.ncd" "${projname}.ngd"
 
-"${XILINX_ISE}/par" -w -mt 4 "${projname}_map.ncd" "${projname}.ncd"
+    "${XILINX_ISE}/par" -w -mt 4 "${projname}_map.ncd" "${projname}.ncd"
 
-# TODO generate timing report?
-#trce -v 3 -s 2 -n 3 -fastpaths -xml hyperdrive.twx hyperdrive.ncd -o hyperdrive.twr hyperdrive.pcf
+    # TODO generate timing report?
+    #trce -v 3 -s 2 -n 3 -fastpaths -xml hyperdrive.twx hyperdrive.ncd -o hyperdrive.twr hyperdrive.pcf
 
-# BitGen options
-cat > "${projname}.ut" << EOF
+    # BitGen options
+    cat > "${projname}.ut" << EOF
 -w
 -g Binary:Yes
 -g Compress
 -g UnusedPin:PullNone
 EOF
 
-"${XILINX_ISE}/bitgen" -f "${projname}.ut" "${projname}.ncd"
+    "${XILINX_ISE}/bitgen" -f "${projname}.ut" "${projname}.ncd"
+}
 
-# Impact options
-cat > "${projname}_impact.cmd" << EOF
+load() {
+    # Impact options
+    cat > "${projname}_impact.cmd" << EOF
 setMode -bs
 setCable -p usb21 -b 12000000
 addDevice -p 1 -file ${projname}.bit
@@ -60,4 +63,9 @@ program -p 1
 exit
 EOF
 
-[[ "$1" == "load" ]] && "${XILINX_ISE}/impact" -batch "${projname}_impact.cmd"
+    "${XILINX_ISE}/impact" -batch "${projname}_impact.cmd"
+}
+
+for action in $@; do
+    $action
+done
